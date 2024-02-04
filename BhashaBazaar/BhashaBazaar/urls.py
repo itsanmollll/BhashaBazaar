5d111fac5bb056ae763a8ee58b248c4b6365e2b4
itsanmollll <anmolbhardwajinv@gmail.com>
2024-02-04 17:34:25 +0530

    Allmost basic project setup

--- BhashaBazaar/BhashaBazaar/urls.py
+++ BhashaBazaar/BhashaBazaar/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
+admin.site.site_header = "Hello BhashaBazaar Admin"
+admin.site.site_title = "Hello BhashaBazaar Portal"
+admin.site.index_title = "Welcome to BhashaBazaar Researcher Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('backend.urls')),
 ]
